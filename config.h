cb71868cf449101baa35c8d297c8d435c4cf73fa
FemboyBruher1337 <101051136+FemboyBruher1337@users.noreply.github.com>
2023-01-07 11:42:41 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "jetbrainsmono:size=10", "icofont:size=12" };
+static const char *fonts[]          = { "jetbrainsmono:size=10", "FontAwesome:size=50" };
 static const char dmenufont[]       = "jetbrainsmono:size=10";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "?" };
 
 static const Rule rules[] = {
 	/* xprop(1):
